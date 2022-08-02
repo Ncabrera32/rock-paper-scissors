428a6488232c2fb9da3baa81c4b6fab9b040935d
Ncabrera32 <nathanielcabrera88@yahoo.com>
2022-08-01 21:26:10 -0600

    added player functions to select Rock Paper or Scissors

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # rock-paper-scissors
 first commit added basic files with boilerplates
-second commit added some basic funcitons for the game in the javascript file
+second commit added some basic functons for the game in the javascript file for computer choice
+second commit added functions to give user input input and if given false info game wont break
--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 const choices = ["rock","paper","scissors"]
 
 function game (){
-   //play the game
-   //play five rounds
-   //console based 
+playRound();
 }
 
 function playRound(){
 const playerSelection = playerChoice()
+const computerSelection = computerChoice()
 }
 
+//player can't break function
 function playerChoice () {
-    //gett input from player
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+}
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    if (check == true) {
+        console.log(input);
+    }
 }
 
-function comptuerChoice(){
+function computerChoice(){
     //get random inout for computer
     return choices[Math.floor(Math.random()*choices.length)]
 }
+
+function validateInput(choice){
+    if (choices.includes(choice)) {
+        return true;
+    } 
+    return false;
+}
+
+
+game();
