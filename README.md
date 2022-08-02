aac19839d08070b07b5d96224c08a0a74bc5fa95
Ncabrera32 <nathanielcabrera88@yahoo.com>
2022-08-01 21:11:36 -0600

    added some starting funcitons to main.js and some info to readme.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+first commit added basic files with boilerplates
+second commit added some basic funcitons for the game in the javascript file
---
+++ main.js
@@ -0,0 +1,20 @@
+const choices = ["rock","paper","scissors"]
+
+function game (){
+   //play the game
+   //play five rounds
+   //console based 
+}
+
+function playRound(){
+const playerSelection = playerChoice()
+}
+
+function playerChoice () {
+    //gett input from player
+}
+
+function comptuerChoice(){
+    //get random inout for computer
+    return choices[Math.floor(Math.random()*choices.length)]
+}
